a1e77c1f7ef3aa8d69fd820b18e3eeaf73bdd505
Nallely20030627 <112960628+Nallely20030627@users.noreply.github.com>
2022-09-27 12:58:10 -0500

    Update and rename HNJC_compara.cpp to compara.cpp

---
+++ compara.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float HNJC_a,HNJC_b;
+	cout<<"ingrese a b:"; cin>>HNJC_a>>HNJC_b;
+	if(HNJC_a>HNJC_b)
+	{
+	  cout<<HNJC_a<<" es mayor a "<<HNJC_b<<endl;
+
+
+	}else{
+
+	  cout<<HNJC_b<<" es mayor a "<<HNJC_a<<endl;
+
+
+	}
+
+	return 0;
+
+
+
+
+
+
+}
