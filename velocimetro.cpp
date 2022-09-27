45cdb104cf7e98388642940237a296f13ffdb825
Nallely20030627 <112960628+Nallely20030627@users.noreply.github.com>
2022-09-27 12:53:28 -0500

    Update and rename HNJC_velocimetro.cpp to velocimetro.cpp

---
+++ velocimetro.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float HNJC_v,HNJC_d,HNJC_t;
+	cout<<"Ingrese la distancia(en km) d= "; cin>>HNJC_d;
+	cout<<"Ingrese el tiempo(en horas)  t= "; cin>>HNJC_t;
+	HNJC_v=HNJC_d/HNJC_t;
+	cout<<"La velocidad calculada es de v= "<<HNJC_t<<endl<<endl;
+	return 0;
+
+
+
+}
